f2bef2863b711bf6d12144bcfd49b2fcb840c89b
Viktor <Burenin777@gmail.com>
2023-10-19 09:24:38 -0400

    Initial Commit

---
+++ Imperica/ImpericaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImpericaApp.swift
+//  Imperica
+//
+//  Created by Viktor on 2023-10-19.
+//
+
+import SwiftUI
+
+@main
+struct ImpericaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
